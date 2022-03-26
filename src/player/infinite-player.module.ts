bd6938e940e012b6ec921225c9ba3697d6d9b3bd
Rhino <a.bazoli@smartrecruiters.com>
2022-03-26 11:55:53 +0100

    feat(button): style buttons

--- src/player/infinite-player.module.ts
+++ src/player/infinite-player.module.ts
@@ -4,12 +4,11 @@ import { ProgressBarComponent } from 'src/progress-bar/progress-bar.component';
 
 import { MusicBoundariesModule } from 'src/music-boundaries/music-boundaries.module';
 import { InfinitePlayerComponent } from './infinite-player.component';
-import { SharedModule } from 'src/shared/shared.module';
 
 @NgModule({
   declarations: [InfinitePlayerComponent, ProgressBarComponent],
   // #todo How can I not need to declare the progress bar component here?
-  imports: [FormsModule, MusicBoundariesModule, SharedModule],
+  imports: [FormsModule, MusicBoundariesModule],
   exports: [InfinitePlayerComponent]
 })
 export class InfinitePlayerModule {}
